3ad59462e6b40014b8e36e1a0b8803092e2bb404
nguyenthung97 <65891848+nguyenthung97@users.noreply.github.com>
2021-04-07 14:29:45 +0700

    update enviroment

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -4,7 +4,7 @@ My name is Vincent Azure. Welcome to my messy Website!
 <br>
 Please click the link below to verify your email and activate your account.
 <br>
-<a href="http://localhost:8000/verify?code={{$email_data['verification_code']}}" > DO NOT CLICK HERE BUT IF YOU WANT TO, THEN CLICK </a>
+<a href="http://lvcrud.herokuapp.com/verify?code={{$email_data['verification_code']}}" > DO NOT CLICK HERE BUT IF YOU WANT TO, THEN CLICK </a>
 <br>
 <br>
 Thank you for joining with me.
